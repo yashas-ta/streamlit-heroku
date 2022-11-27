789a5bf035bddcccdf1611e0e6151f5a07d67140
yashas-ta <119160329+yashas-ta@users.noreply.github.com>
2022-11-27 17:04:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 
-eco"\
+free"\
 [general]\n\
 email = \"21f1006838@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 
-eco "\
+free "\
 [server]\n\
 headless = true\n\
 enableCORS = false\n\
