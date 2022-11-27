bb9a3cd0b830c1b8b8b3ba457d43dd99ce01f291
yashas-ta <119160329+yashas-ta@users.noreply.github.com>
2022-11-27 16:04:04 +0530

    code to find the largest of three

---
+++ largest-of-three.py
@@ -0,0 +1,17 @@
+!pip install streamlit
+
+import streamlit as st
+import numpy as np
+
+st.title('Find the largest of three numbers')
+
+st.write('Input three numbers below:')
+
+n1 = st.number_input('Enter the first number')
+n2 = st.number_input('Enter the second number')
+n3 = st.number_input('Enter the third number')
+
+numbers = np.array((n1,n2,n3))
+largest = np.amax(numbers)
+
+st.write('The largest number is:',largest)
